ae9f21f54e81bb3699129250d8f507aa71c352e6
AdityaNurgraha <aditngrh11@gmail.com>
2025-12-25 19:37:18 +0700

    fix: send booking confirmation email synchronously and include booking details

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,19 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+   /* server: {
+        host: '0.0.0.0',
+        port: 8080,
+        strictPort: true,
+        hmr: {
+            host: '192.168.1.7',
+            port: 8080,
+        },
+    },*/
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            // pastikan dev server URL tidak aktif
-            devServer: {
-                https: true,
-            }
         }),
     ],
-    base: '/build/', // relative path → otomatis HTTPS
 });
